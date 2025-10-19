aa01930792c0b3e40a67c7de250e5d8fce16e479
Luiz Felipe Oliveira <luiz.rya@gmail.com>
2025-10-19 16:10:04 -0300

    add comment to hello.asm

--- hello.asm
+++ hello.asm
@@ -1,12 +1,14 @@
 global _start
 
+;defino variavel
 section  .data
-message: db 'hello, word!',10
+    ;db = define byte / 10 = \n
+    message: db 'hello, word!',10
 
 section .text
 _start:
-  mov   rax,1
-  mov   rdi,1
-  mov   rsi,message
-  mov   rdx,14
-  syscall
+  mov   rax,1        ;armgumento para executra o syscall
+  mov   rdi,1        ;informo para enviar para o stdout
+  mov   rsi,message  ;aponto para o endereço onde esta armazenado a mensagem
+  mov   rdx,14       ;informo a quantidades de bytes
+  syscall            ;chamo o syscall
